dc9805993c12a6287d674c53a8d2eabc1a3156b1
Francis Carino <fgcarino@gmail.com>
2022-08-23 16:27:07 -0700

    initial commit an complete problem 1

---
+++ routes/shipments.test.js
@@ -0,0 +1,41 @@
+"use strict";
+
+const request = require("supertest");
+const app = require("../app");
+
+describe("POST /", function () {
+  test("valid", async function () {
+    const resp = await request(app).post("/shipments").send({
+      productId: 1000,
+      name: "Test Tester",
+      addr: "100 Test St",
+      zip: "12345-6789",
+    });
+
+    expect(resp.body).toEqual({ shipped: expect.any(Number) });
+  });
+});
+
+describe("POST /", function () {
+  test("invalid input", async function () {
+    const resp = await request(app).post("/shipments").send({
+      productId: 200,
+      name: 123,
+      addr: 123,
+      zip: true,
+    });
+
+    expect(resp.statusCode).toEqual(400);
+    expect(resp.body).toEqual({
+      error: {
+        message: [
+          "instance.productId must be greater than or equal to 1000",
+          "instance.name is not of a type(s) string",
+          "instance.addr is not of a type(s) string",
+          "instance.zip is not of a type(s) string",
+        ],
+        status: 400,
+      },
+    });
+  });
+});
